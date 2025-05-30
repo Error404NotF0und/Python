1f88c46dd378782b1c933ac0abf0ce21fae77927
GlitchCoder <83817711+Error404NotF0und@users.noreply.github.com>
2025-05-30 21:25:28 +0600

    Add files via upload

---
+++ Basic Projects/dictionary.py
@@ -0,0 +1,31 @@
+empty_dict = {}
+
+
+integer_dict = {1: 'apple', 2: 'ball'}
+
+
+mixed_dict = {'name': 'John', 1: [2,4,3]}
+
+mixed2_dict = {'name': 'Jack', 'age': 26}
+
+print(mixed2_dict.get('name'))
+print(mixed2_dict.get('age'))
+
+
+
+mixed2_dict['age'] = 27
+print(mixed2_dict)
+
+mixed2_dict['Address'] = 'Downtown'
+print(mixed2_dict)
+
+mixed_dict.pop('age')
+print(mixed2_dict)
+
+print("Address :", mixed2_dict.get('address'))
+
+mixed_dict.clear()
+print(mixed_dict)
+
+
+
---
+++ Basic Projects/lists.py
@@ -0,0 +1,28 @@
+list = ['Mango', 'Banana', 'Orange', 'Grape', 'Dragonfruit']
+
+print("length of list", len(list))
+print("First Element:", list[0])
+print("Last Element:", list[-1])
+
+list.append('Pineapple')
+print("Updated List :\n", list)
+
+list.remove('Grape')
+print("Updated List :\n", list)
+
+list.sort()
+print("Sorted List:\n", list)
+
+list.pop(1)
+print("Updated List :\n", list)
+
+list.reverse()
+print("Reversed List :\n", list)
+
+print("Multiplication on List :\n", list*2)
+
+list = list[:4]
+print("Sliced List :\n", list)
+
+list.clear()
+print("Updated List :\n", list)
